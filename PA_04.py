6ef5dbf1557f8b8557524dfc7393e9c0b2f48cbc
attila kekesi <kekesi.att@gmail.com>
2020-12-14 14:32:11 +0100

    Update PA_04.py
    statt    qa = qa + ...
    lieber  qa += ... (besser, fuer spaetere HA)
    zus. Kommentar fuer Abgabe

--- PA_04.py
+++ PA_04.py
@@ -1,11 +1,16 @@
 # CoMa - PA_04
+# Attila Kekesi (402830)
 
 # Hilfsfunktion_1
+# Berechnung für quadratischen Abstand
+# Input: Liste von Punkten_(x,y), Linie_(a,b)
+# Output: quadratischer Abstand
 def get_linedistance(points,line):
     qa = 0
     for i in range(len(points)):
-        qa = qa + (line[0]*points[i][0]+line[1]-points[i][1])**2
-    return qa
+        #qa = qa + (line[0]*points[i][0]+line[1]-points[i][1])**2   # lieber mit qa += ...
+        qa += (line[0]*points[i][0]+line[1]-points[i][1])**2
+    return qa  
 
 # Testfunktion: get_linedistance(points,line)
 def test_get_linedistance():
@@ -23,6 +28,9 @@ def test_get_linedistance():
 #test_get_linedistance()
 
 # Hilfsfunktion_2
+# Berechnung für kleinsten Wert von einer Liste
+# Input: Liste von Zahlen
+# Output: kleinster Wert von der Liste
 def get_min(int_list):
     L = len(int_list)
     if L == 0:
@@ -43,6 +51,9 @@ def test_get_min():
 
 
 # Hauptfunktion
+# Berechnung für kleinsten quadratischen Abstand
+# Input: Liste von Punkten_(x,y), Liste von Linien_(a,b)
+# Output: kleinster quadratischer Abstand
 def linear_regression(points,lines):
     R = []
     for i in range(len(lines)):
@@ -56,9 +67,9 @@ def test_linear_regression():
     Test_lines = [[(1,1),(-1,2)],[(-1,2)],[(1,1)],[(0,0)],[(1,1)],[(0,0)]]
     for i in range(len(Test_points)):
         for j in range(len(Test_lines)):
-            print(i)
-            print(j)
-            print(Test_lines[j])
-            print(Test_points[i])
+            #print(i)
+            #print(j)
+            #print(Test_lines[j])
+            #print(Test_points[i])
             print(linear_regression(Test_points[i],Test_lines[j]))
 #test_linear_regression()

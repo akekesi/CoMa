23e99da9b7406cf1d937e89facc3d398e2907e35
attila kekesi <kekesi.att@gmail.com>
2021-06-07 20:36:04 +0200

    sorted

--- CoMa_II_PA_07_unittest.py
+++ CoMa_II_PA_07_unittest.py
@@ -17,7 +17,8 @@ class Test(unittest.TestCase):
         self.assertEqual(T.n_walls(),1)
         S = T.flip([0,2])
         self.assertEqual(S.triangles,[[0, 1, 3], [1, 2, 3]])
-        self.assertEqual(T.triangles,[[0, 1, 2], [0, 2, 3]])
+        self.assertEqual(S.walls,[[1,3]])
+        self.assertEqual(T.triangles,triangles)
 
     def test_01(self):
         # Test von HA07
@@ -34,6 +35,11 @@ class Test(unittest.TestCase):
         self.assertEqual(T.triangles,triangles)
         self.assertEqual(T.walls,[])
         self.assertEqual(T.n_walls(),0)
+        S = T.flip([])
+        self.assertEqual(S.triangles,triangles)
+        self.assertEqual(S.walls,[])
+        self.assertEqual(T.triangles,triangles)
+
 
     def test_03(self):
         n = 5
@@ -45,8 +51,9 @@ class Test(unittest.TestCase):
         self.assertEqual(T.walls,[[0,2],[0,3]])
         self.assertEqual(T.n_walls(),2)
         S = T.flip([0,2])
-        self.assertEqual(S.triangles,[[0,1,3],[1,2,3],[0,3,4]])
-        self.assertEqual(T.triangles,[[0,1,2],[0,2,3],[0,3,4]])
+        self.assertEqual(S.triangles,[[0,1,3],[0,3,4],[1,2,3]])
+        self.assertEqual(S.walls,[[0,3],[1,3]])
+        self.assertEqual(T.triangles,triangles)
 
     def test_04(self):
         n = 5
@@ -59,7 +66,8 @@ class Test(unittest.TestCase):
         self.assertEqual(T.n_walls(),2)
         S = T.flip([0,2])
         self.assertEqual(S.triangles,[[0,1,4],[1,2,4],[2,3,4]])
-        self.assertEqual(T.triangles,[[0,1,2],[0,2,4],[2,3,4]])
+        self.assertEqual(S.walls,[[1,4],[2,4]])
+        self.assertEqual(T.triangles,triangles)
 
     def test_05(self):
         n = 6
@@ -70,7 +78,10 @@ class Test(unittest.TestCase):
         self.assertEqual(T.triangles,triangles)
         self.assertEqual(T.walls,[[0,2],[0,3],[0,4]])
         self.assertEqual(T.n_walls(),3)
-        # flip
+        S = T.flip([0,3])
+        self.assertEqual(S.triangles,[[0,1,2],[0,2,4],[0,4,5],[2,3,4]])
+        self.assertEqual(S.walls,[[0,2],[0,4],[2,4]])
+        self.assertEqual(T.triangles,triangles)
 
     def test_06(self):
         n = 6
@@ -79,14 +90,31 @@ class Test(unittest.TestCase):
         print(f'Triangles:\t{T.triangles}')
         print(f'Walls:\t\t{T.walls}')
         self.assertEqual(T.triangles,triangles)
-        self.assertEqual(T.walls,[[0,2],[2,4],[0,4]])
+        self.assertEqual(T.walls,[[0,2],[0,4],[2,4]])
         self.assertEqual(T.n_walls(),3)
-        # flip
+        S = T.flip([2,4])
+        self.assertEqual(S.triangles,[[0,1,2],[0,2,3],[0,3,4],[0,4,5]])
+        self.assertEqual(S.walls,[[0,2],[0,3],[0,4]])
+        self.assertEqual(T.triangles,triangles)
 
     def test_07(self):
         n = 6
         triangles = [[0,1,2],[0,2,3],[0,4,5],[2,3,4]]
         with self.assertRaises(Exception): ngonTriang(n,triangles)
+
+    def test_08(self):
+        n = 8
+        triangles = [[0,1,7],[1,3,7],[1,2,3],[3,6,7],[3,4,5],[3,5,6]]
+        T = ngonTriang(n,triangles)
+        print(f'Triangles:\t{T.triangles}')
+        print(f'Walls:\t\t{T.walls}')
+        self.assertEqual(T.triangles,sorted(triangles))     # triangles war nicht sorted
+        self.assertEqual(T.walls,[[1,3],[1,7],[3,5],[3,6],[3,7]])
+        self.assertEqual(T.n_walls(),5)
+        S = T.flip([3,6])
+        self.assertEqual(S.triangles,[[0,1,7],[1,2,3],[1,3,7],[3,4,5],[3,5,7],[5,6,7]])
+        self.assertEqual(S.walls,[[1,3],[1,7],[3,5],[3,7],[5,7]])
+        self.assertEqual(T.triangles,sorted(triangles))     # triangles war nicht sorted
 
 if __name__ == "__main__":
     unittest.main()
